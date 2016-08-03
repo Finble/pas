350a24520a6f2891e6dbb07727a05b17800b88c7
Finble <elainefinn@hotmail.com>
2016-08-03 14:30:50 +0100

    set up x2 get routes, get route + get about, tested on port 3000

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+web-server app:
+    node.js app using express (to provide server)
+
+dependencies:  
+    express (v 4.13.2)
+    
+```
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (request, response) {  // '/' root URL  get = http method, so getting info via request, with a response back from get
-    response.send('Hello Express!');
+    response.send('Hello Express!');  // response.send can send string back
+});
+
+app.get('/about', function (request, response) {
+    response.send('About us');  // response.send can send string back
 });
 
 app.listen(3000); // specifies port to be listened on
