dd23ed59cea826613ea51bfc468b12a1edf29b69
Finble <elainefinn@hotmail.com>
2016-08-03 15:29:39 +0100

    added ROUTE-LEVEL MIDDLEWARE authentication to SPECIFIC about route + APPLICATION MIDDLEWARE logger to show http method for EVERY route

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;   
+var PORT = 3000; 
 
-app.get('/about', function (request, response) {
+// route level middleware - applies to SOME routes
+
+var middleware = {
+  requireAuthentication: function (req, res, next) {
+      console.log('private route hit!'); // prints only when route input into browser
+      next();
+  },
+    logger: function (req, res, next) {
+        console.log('Request: ' + req.method + '' + req.originalUrl);  // prints out http method, e.g. get + route, so get/index.html or get/about - so shows which requests are being made to the server
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication); //.use is a way of calling middleware, order is important here, this must be BEFORE other routes
+
+app.get('/about', middleware.requireAuthentication, function (request, response) { // adds route-level middleware as requireAuthentication is only called when about route is put in browser
     response.send('About us');  
 });
 
